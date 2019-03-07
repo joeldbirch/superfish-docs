74b521b38e4f77742e8c4caa23404e32d7610752
Joel Birch <joeldbirch@gmail.com>
2019-03-07 20:59:21 +1100

    Style 404 page and sticky footer.

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,10 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="content section">
+      <h1 className="is-5">Not found</h1>
+      <p>You just hit a route that doesn&#39;t exist… the sadness.</p>
+    </div>
   </Layout>
 )
 
